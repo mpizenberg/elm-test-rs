209971269eb440f42b4d78db27e3e346db947ad5
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-04-01 16:47:28 +0200

    CLI: manage --version flag

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use seahorse::{App, Flag, FlagType};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let app = App::new()
+        .name("elm-test-rs")
+        .author(std::env!("CARGO_PKG_AUTHORS"))
+        .description(std::env!("CARGO_PKG_DESCRIPTION"))
+        .usage("elm-test-rs [command] [arg]")
+        .version(std::env!("CARGO_PKG_VERSION"))
+        .action(default)
+        .flag(Flag::new("version", "elm-test-rs --version(-v)", FlagType::Bool).alias("v"));
+    app.run(args);
+}
+
+fn default(context: &seahorse::Context) {
+    if context.bool_flag("version") {
+        print_version();
+    } else {
+        println!("{:?}", context.args);
+    }
+}
+
+fn print_version() {
+    println!("{}", std::env!("CARGO_PKG_VERSION"));
 }
