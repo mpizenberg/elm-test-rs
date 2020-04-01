d3ed83ba43f36638fd5c177076f070b56416c380
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-04-01 17:00:16 +0200

    CLI: manage init command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use seahorse::{App, Flag, FlagType};
+use seahorse::{App, Command, Flag, FlagType};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -8,19 +8,32 @@ fn main() {
         .description(std::env!("CARGO_PKG_DESCRIPTION"))
         .usage("elm-test-rs [command] [arg]")
         .version(std::env!("CARGO_PKG_VERSION"))
-        .action(default)
-        .flag(Flag::new("version", "elm-test-rs --version(-v)", FlagType::Bool).alias("v"));
+        .action(default_action)
+        .flag(Flag::new("version", "elm-test-rs --version(-v)", FlagType::Bool).alias("v"))
+        .command(init_command());
     app.run(args);
 }
 
-fn default(context: &seahorse::Context) {
+fn default_action(context: &seahorse::Context) {
     if context.bool_flag("version") {
         print_version();
     } else {
-        println!("{:?}", context.args);
+        println!("args: {:?}", context.args);
     }
 }
 
 fn print_version() {
     println!("{}", std::env!("CARGO_PKG_VERSION"));
+}
+
+fn init_command() -> Command {
+    Command::new()
+        .name("init")
+        .usage("elm-test-rs init [arg]")
+        .action(init_action)
+}
+
+fn init_action(context: &seahorse::Context) {
+    println!("TODO: init command");
+    println!("args: {:?}", context.args);
 }
