d340cb7b606ae880951a458446e3083bcadbdc97
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-04-02 15:44:42 +0200

    Return unique paths in glob example

--- examples/glob.rs
+++ examples/glob.rs
@@ -1,18 +1,19 @@
 use glob::glob;
+use std::collections::HashSet;
+use std::path::PathBuf;
 
 /// usage: cargo run --example glob -- "src/**/*" "examples/**/*"
 pub fn main() {
-    for entry in std::env::args()
+    let files: HashSet<PathBuf> = std::env::args()
         // skip program name
         .skip(1)
         // join globs for each command line argument
         .flat_map(|pattern| glob(&pattern).expect("Failed to read glob pattern"))
-    {
-        match entry {
-            // canonical, absolute form of the path
-            // with all intermediate components normalized and symbolic links resolved
-            Ok(path) => println!("{:?}", path.canonicalize().expect("woops")),
-            Err(e) => println!("{:?}", e),
-        }
-    }
+        // filter out errors
+        .filter_map(|x| x.ok())
+        // canonicalize in absolute form
+        .map(|x| x.canonicalize().expect("Error in canonicalize"))
+        // collect into a set of unique values
+        .collect();
+    files.iter().for_each(|f| println!("{:?}", f));
 }
