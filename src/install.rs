bf4ae06e3ac2a01b2d9433df502e387972bde7a9
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-04-11 13:27:58 +0200

    Add suggestion message to use elm-json for install

--- src/install.rs
+++ src/install.rs
@@ -1,6 +1,9 @@
 use std::process::Command;
 
 pub fn main(packages: Vec<String>) {
+    // Recommend direct usage of elm-json instead
+    println!("Don't hesitate to try zwilias/elm-json directly instead!");
+
     // Install packages in test dependencies
     let _ = Command::new("elm-json")
         .arg("install")
