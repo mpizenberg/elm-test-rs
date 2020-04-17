aa360eed593db0e7f8826aa99da044c5e908aa2e
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-04-17 16:03:35 +0200

    Remove examples

--- examples/supervisor/runner.js
+++
@@ -1,6 +0,0 @@
-const { parentPort } = require("worker_threads");
-
-parentPort.on("message", (msg) => {
-  const success = Math.random() >= 0.5;
-  parentPort.postMessage({ type: "TEST_RESULT", id: msg.id, success: success });
-});
