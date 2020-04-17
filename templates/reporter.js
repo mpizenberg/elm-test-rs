76e0284984c61fb3517f7a1b3ba15da9acad8539
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-04-17 11:04:17 +0200

    Update some doc comments

--- templates/reporter.js
+++ templates/reporter.js
@@ -2,7 +2,7 @@
 {{ polyfills }}
 
 const Elm = (function(module) {
-  // Compiled by elm-test-rs from templates/Runner.elm
+  // Compiled by elm-test-rs from templates/Reporter.elm
   {{ compiled_elm }}
   return this.Elm;
 })({});
@@ -18,7 +18,7 @@ const app = Elm.Reporter.init({ flags: flags });
 // Pipe the Elm stdout port to stdout
 app.ports.stdout.subscribe((str) => process.stdout.write(str));
 
-// Export function to set the callback function when reports are finished
+// Export function to set the callback function used when reports are finished
 let finishCallback = () => console.error("finishCallback not defined yet");
 app.ports.signalFinished.subscribe((code) => finishCallback(code));
 exports.setCallback = (callback) => { finishCallback = callback; };
@@ -26,5 +26,5 @@ exports.setCallback = (callback) => { finishCallback = callback; };
 // Export function to restart the Elm reporter
 exports.restart = app.ports.restart.send;
 
-// Export function to send results to Elm
+// Export function to send results to the Elm reporter
 exports.sendResult = app.ports.incomingResult.send;
