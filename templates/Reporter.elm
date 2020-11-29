98ac4769a32734c0e24ffd76d8ec23a7fe9c16cb
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-11-29 23:16:19 +0100

    Format Reporter.elm template

--- templates/Reporter.elm
+++ templates/Reporter.elm
@@ -3,10 +3,18 @@ port module Reporter exposing (main)
 import ElmTestRunner.Reporter exposing (Flags, Model, Msg)
 import Json.Decode exposing (Value)
 
+
 port restart : (Int -> msg) -> Sub msg
+
+
 port incomingResult : (Value -> msg) -> Sub msg
+
+
 port signalFinished : Int -> Cmd msg
+
+
 port stdout : String -> Cmd msg
+
 
 main : Program Flags Model Msg
 main =
