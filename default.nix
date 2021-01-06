25635ff0612386d781cc67c5b6be076a2ecd80fb
Brian Hicks <brian@brianthicks.com>
2021-01-06 05:54:17 -0600

    add some maintenance info to default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,21 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
+# how to keep this file up to date:
+#
+# 1. keep the `version` in sync with the version in `cargo.toml`. Nothing bad
+#    will happen if you don't do this, but it's nicer to get bug reports
+# 2. rebuild with nix whenever you update `cargo.lock` to get the new
+#    `cargoSha256`. It will give you a more useful error if you replace the hash
+#    with all 0s before building.
+#
+# In the longer term, this file could be submitted to nixpkgs to make it easier
+# for Nix users to install elm-test-rs. That's probably the right move once it
+# hits 1.0.0!
+
 let macosDeps = [ pkgs.darwin.apple_sdk.frameworks.CoreServices ];
 in pkgs.rustPlatform.buildRustPackage {
   pname = "elm-test-rs";
   version = "0.5.1";
-
-  buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin macosDeps;
 
   # a nice addition here might be https://github.com/hercules-ci/gitignore.nix to
   # ignore files from git, which would prevent unnecessary rebuilds. But since
@@ -13,8 +23,8 @@ in pkgs.rustPlatform.buildRustPackage {
   # dependency!
   src = ./.;
 
-  # to update this, set the string to all 0's, rebuild, and grab the new hash
-  # that nix gives you
+  buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin macosDeps;
+
   cargoSha256 = "1p3fyzs5bkvyvzm5ns3azjb82m5dsafy2c481rxkm00vanadk1mi";
   verifyCargoDeps = true;
 }
