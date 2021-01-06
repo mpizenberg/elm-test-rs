d9ccde1dda192e11dce5ae356a52d6757d48a403
Brian Hicks <brian@brianthicks.com>
2021-01-06 08:18:31 -0600

    add packaging for nix (#48)
    * package using Nix
    
    * add some maintenance info to default.nix

---
+++ default.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> { }, ... }:
+
+# how to keep this file up to date:
+#
+# 1. keep the `version` in sync with the version in `cargo.toml`. Nothing bad
+#    will happen if you don't do this, but it's nicer to get bug reports
+# 2. rebuild with nix whenever you update `cargo.lock` to get the new
+#    `cargoSha256`. It will give you a more useful error if you replace the hash
+#    with all 0s before building.
+#
+# In the longer term, this file could be submitted to nixpkgs to make it easier
+# for Nix users to install elm-test-rs. That's probably the right move once it
+# hits 1.0.0!
+
+let macosDeps = [ pkgs.darwin.apple_sdk.frameworks.CoreServices ];
+in pkgs.rustPlatform.buildRustPackage {
+  pname = "elm-test-rs";
+  version = "0.5.1";
+
+  # a nice addition here might be https://github.com/hercules-ci/gitignore.nix to
+  # ignore files from git, which would prevent unnecessary rebuilds. But since
+  # this is just for packaging for now, no need to bother with managing the
+  # dependency!
+  src = ./.;
+
+  buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin macosDeps;
+
+  cargoSha256 = "1p3fyzs5bkvyvzm5ns3azjb82m5dsafy2c481rxkm00vanadk1mi";
+  verifyCargoDeps = true;
+}
