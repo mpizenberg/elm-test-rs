0288b016969c9335d955366fca8a34329b42f68d
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-02-09 18:51:52 +0100

    Fix easy issues with the init command

--- src/init.rs
+++ src/init.rs
@@ -8,14 +8,17 @@ use pubgrub_dependency_provider_elm::project_config::ProjectConfig;
 /// and initialize a template tests/Tests.elm file.
 pub fn main() -> anyhow::Result<()> {
     // Install elm-explorations/test in the tests dependencies
-    let elm_json_str = std::fs::read_to_string("elm.json").context("Unable to read elm.json")?;
+    let project_root = crate::utils::elm_project_root("")?;
+    let elm_json_path = project_root.join("elm.json");
+    let elm_json_str =
+        std::fs::read_to_string(&elm_json_path).context("Unable to read elm.json")?;
     let project_config: ProjectConfig =
         serde_json::from_str(&elm_json_str).context("Invalid elm.json")?;
     let updated_config = crate::deps::init(project_config).context(
-        "Something went wrong when installing elm-explorations/test to the tests depedencies",
+        "Something went wrong when installing elm-explorations/test to the tests dependencies",
     )?;
     std::fs::write(
-        "elm.json",
+        &elm_json_path,
         serde_json::to_string_pretty(&updated_config)
             .context("Unable to convert the config to a JSON string")?,
     )
@@ -23,12 +26,13 @@ pub fn main() -> anyhow::Result<()> {
 
     // Create the tests/Tests.elm template
     let init_tests_template = include_template!("Tests.elm");
-    std::fs::create_dir_all("tests").context("Impossible to create directory tests/")?;
-    let new_file_path = std::path::Path::new("tests").join("Tests.elm");
+    let tests_dir = project_root.join("tests");
+    std::fs::create_dir_all(&tests_dir).context("Impossible to create directory tests/")?;
+    let new_file_path = tests_dir.join("Tests.elm");
     if !new_file_path.exists() {
         std::fs::write(new_file_path, init_tests_template)
             .context("Unable to create Tests.elm template")?;
-        eprintln!("The file test/Tests.elm was created");
+        eprintln!("The file tests/Tests.elm was created");
     }
     Ok(())
 }
