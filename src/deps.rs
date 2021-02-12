55493f5a5540f3ca4ae617ca17796ea5b4b6663d
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-02-12 18:27:11 +0100

    Fix forgotten elm version change

--- src/deps.rs
+++ src/deps.rs
@@ -233,7 +233,7 @@ fn solve_helper<P: AsRef<Path>>(
     let mut deps = direct_deps;
     deps.insert(
         "mpizenberg/elm-test-runner".to_string(),
-        Range::exact((4, 0, 1)),
+        Range::exact((4, 0, 2)),
     );
     // Add elm/json to the deps since it's used in Runner.elm and Reporter.elm.
     if !deps.contains_key("elm/json") {
