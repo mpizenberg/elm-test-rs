5b8ff4022757aeb7e0c94bbf560d258a94bde61e
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-11-03 00:51:54 +0100

    Save downloaded versions index

--- src/deps.rs
+++ src/deps.rs
@@ -352,6 +352,12 @@ fn solve_deps(
                 Ok(provider) => provider,
                 Err(e) => anyhow::bail!("Failed to initialize the online provider.\n{}", e,),
             };
+            online_provider.save_cache().map_err(|err| {
+                anyhow::anyhow!(
+                    "Failed to save downloaded cache from package website\n{}",
+                    err
+                )
+            })?;
             // TODO: Improve the pubgrub_dependency_provider_elm package to have
             // correctly implemented errors with thiserror.
             let deps_provider =
